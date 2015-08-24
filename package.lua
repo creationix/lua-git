b4016eb81229af0cb0db88a5d352df2696eaf277
Tim Caswell <tim@creationix.com>
2015-08-24 12:28:26 -0500

    Publish 2.0.4

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "creationix/git",
-  version = "2.0.3",
+  version = "2.0.4",
   homepage = "https://github.com/creationix/lua-git",
   description = "An implementation of git in pure lua.",
   tags = {"git","db","codec"},
