bb14126d9642c00d61b3b6000dda2a0fe8e3dafd
Tim Caswell <creationix@vercel.com>
2021-08-27 12:59:24 -0500

    Bump version

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "creationix/git",
-  version = "2.0.7",
+  version = "2.1.0",
   homepage = "https://github.com/creationix/lua-git",
   description = "An implementation of git in pure lua.",
   tags = {"git","db","codec"},
